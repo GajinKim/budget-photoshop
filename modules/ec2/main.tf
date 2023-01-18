ac0d452869a3dfb0ea46a0fc1a7911a171e8efe7
Gajin Kim <gajin2000@gmail.com>
2023-01-18 13:45:27 -0600

    modules

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,17 +1,34 @@
-data "aws_ami" "amazon_linux_2" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
 }
 
+# resource "tls_private_key" "website_key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
+
+# resource "aws_key_pair" "website_key_pair" {
+#   key_name   = "GajinsWebsiteKey"
+#   public_key = tls_private_key.website_key.public_key_openssh
+# }
+
 resource "aws_instance" "website" {
-  ami                    = data.aws_ami.amazon_linux_2.id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [var.website_security_group_id]
+  key_name               = "GajinsKey"
   user_data              = file("${path.module}//createwebsite.sh") # script runs whenever the EC2 Instance is launched
 
   # attaching storage (EBS)
--- modules/security/main.tf
+++ modules/security/main.tf
@@ -43,6 +43,15 @@ resource "aws_security_group" "website_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    # https port allowed from any ip
+    description = "http"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description      = "all-open"
     from_port        = 0
@@ -60,5 +69,5 @@ resource "aws_security_group" "website_security_group" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-  
+
 }
